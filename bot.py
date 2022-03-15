2f9a3b5ecd06445ada2d9b7dbc67fb0d81cabefb
iyume <iyumelive@gmail.com>
2022-03-15 20:21:28 +0800

    Porject initial

---
+++ bot.py
@@ -0,0 +1,12 @@
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter
+
+nonebot.init()
+
+nonebot.get_driver().register_adapter(Adapter)
+
+nonebot.load_plugin("nonebot_plugin_wordle")
+
+
+if __name__ == "__main__":
+    nonebot.run()
