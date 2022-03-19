794aa5bfdf6ca7907613c4f62c98de936b8db798
iyume <iyumelive@gmail.com>
2022-03-19 21:14:37 +0800

    sync: add get answer util

--- nonebot_plugin_wordle/util.py
+++ nonebot_plugin_wordle/util.py
@@ -1,4 +1,5 @@
 import base64
+from datetime import datetime
 from io import BytesIO
 
 from PIL import Image
@@ -9,3 +10,10 @@ def im2base64(im: Image.Image) -> str:
     buffer = BytesIO()
     im.save(buffer, format="PNG")
     return f"base64://{base64.b64encode(buffer.getvalue()).decode()}"
+
+
+def get_answer() -> str:
+    """Get today's wordle game answer."""
+    from .consts import Ma
+
+    return Ma[(datetime.now() - datetime(2021, 6, 19)).days]
